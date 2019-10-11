59d5ece60bc6570f74a9148163ba3cc5d1883a38
QWxs <2651879595@qq.com>
2019-10-11 20:18:24 +0800

    添加项目文件。

---
+++ EXCEL操作/Form1.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Excel;
+
+namespace EXCEL操作
+{
+    public partial class Form1 : Form
+    {
+        public bool inse_Switch = false;
+        public Form1()
+        {
+          
+            InitializeComponent();
+
+
+
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+             
+        }
+        //创建表格
+        private void button5_Click(object sender, EventArgs e)
+        {
+            var File_Path = "学生信息.xls";
+            string sql = "CREATE TABLE 学生信息([学号] INT,[姓名] VarChar,[班级] VarChar,[电话号码] VarChar,[状态] VarChar)";
+            //执行SQL语句
+            Excel2.Update(sql, File_Path);
+        }
+        //查询
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //判断输入框是否为空
+            if(this.textBox1.Text=="")
+            {
+                //查询全部
+                //Excel路径
+                var File_path = "学生信息.xls";
+                //构建SQL语句
+                string sql = "select 学号,姓名,班级,电话号码 from [学生信息$]  where 状态 = '正常'";
+                //执行SQL语句
+                this.dataGridView1.DataSource = Excel.Excel2.GetDataTable(sql, File_path);
+            }
+            else
+            {
+                //按照条件查询
+                //Excel路径
+                var File_path = "学生信息.xls";
+                //构建SQL语句
+                string sql = "select 学号,姓名,班级,电话号码 from [学生信息$]  where 状态 ="+this.textBox1.Text;
+                //执行SQL语句
+                this.dataGridView1.DataSource = Excel.Excel2.GetDataTable(sql, File_path);
+            }
+        }
+        //插入数据
+        private void button2_Click(object sender, EventArgs e)
+        {
+            inse_Switch = true;
+            this.groupBox1.Enabled = true;
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        //提交数据
+        private void button6_Click(object sender, EventArgs e)
+        {
+            //Excel路径
+            var File_Path = "学生信息.xls";
+            //SQL语句
+            string sql = "";
+            //
+            if (this.textBox2.Text.Trim() == "")
+
+	{
+		 MessageBox.Show("请输入姓名");
+                return;
+	}
+            if (inse_Switch==true)
+            {
+                //SQL语句//插入数据
+                sql = "insert into [学生信息$](学号,姓名,班级,电话号码,状态)  values ({0},'{1}','{2}','{3}','{4}')";
+                sql = string.Format(sql, this.textBox2.Text, this.textBox3.Text, this.textBox4.Text, this.textBox5.Text, "正常");
+                inse_Switch = false;
+            }
+            else
+            {
+                //修改数据
+                sql = "update [学生信息$] set  姓名 ='{0}' ,班级 = '{1}',电话号码= '{2}',状态 = '正常'   where   学号 = {3}";
+                sql = string.Format(sql, this.textBox3.Text, this.textBox4.Text, this.textBox5.Text, this.textBox1.Text);
+            }
+
+           
+            //提交
+            Excel2.Update(sql, File_Path);
+            //查询全部信息
+            this.textBox1.Text = "";//赋值为空，查询全部信息
+            button1_Click(null, null);//触发按钮，查询信息
+
+        }
+        //删除按钮//其实在文件中还是存在的
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //指定文件位置
+            var filepath = "学生信息.xls";
+            //判断学号是否为空
+            if (this.textBox1.Text.Trim() == "")
+            {
+                MessageBox.Show("请输入学号");
+                return;
+            }
+            else
+            {
+                //构建查询语句
+                string sql = "update [学生信息$] set 状态 = '删除' where 学号 = {0}";
+                sql = string.Format(sql, this.textBox1.Text);
+                //更新数据
+                Excel2.Update(sql, filepath);
+                //清空查询编号
+                this.textBox1.Text = "";
+                //执行查询操作
+                button1_Click(null, null);
+            }
+        }
+        //修改
+        private void button4_Click(object sender, EventArgs e)
+        {
+            
+            this.groupBox1.Enabled = true;
+        }
+    }
+}
---
+++ Excel/Class1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//引用数据库操作类
+using System.Data;
+using System.Data.OleDb;
+
+namespace Excel
+{
+    public class Excel2
+    {
+        //在根目录下的bin文件下创建一个Excel表格
+        
+        public static int Update (string sql,string path)
+        {
+            //构建连接语句
+            string sConnectionString = 
+                "Provider = Microsoft.ACE.OLEDB.12.0;" + 
+                "Data Source = " + path + ";" + 
+                "Extended Properties = 'Excel 8.0;HDR = Yes;IMEX = 0'";
+            //IMEX=0 为汇出模式，这个模式Excle只能用作"写入"用途
+            //IMEX=1 为汇入模式，这个模式Excle只能用作"读取"用途
+            //IMEX=2 为链接模式, 这个模式Excle同时支持"读写"用途
+            //HDR=Yes 创建表头
+            //使用连接语句连接Excel-连接上数据库Connection
+            using (OleDbConnection ole_cnn = new OleDbConnection(sConnectionString))
+            {
+                //打开链接
+                ole_cnn.Open();
+                //创建编辑器开始编辑SQL命令
+                using (OleDbCommand ole_cmd = ole_cnn.CreateCommand())
+                {
+                    //执行sql语句
+                    ole_cmd.CommandText = sql;
+                    //执行SQL命令- 返回受影响的函数
+                    return ole_cmd.ExecuteNonQuery();
+                }
+            }
+            
+        }
+
+        //输入在数据库里面查询到的内容，再通过Excel表格输出
+        public static DataTable GetDataTable(string sql,string path)
+        {
+            //构建连接语句
+            string sConnectionString =
+                "Provider = Microsoft.ACE.OLEDB.12.0;" +
+                "Data Source = " + path + ";" +
+                "Extended Properties = 'Excel 8.0;HDR = Yes;IMEX = 0'";
+            //使用连接语句连接Excel-连接上数据库Connection
+            using (OleDbConnection ole_cnn = new OleDbConnection(sConnectionString))
+            {
+                //打开链接
+                ole_cnn.Open();
+                //创建编辑器开始编辑SQL命令
+                using (OleDbCommand ole_cmd = ole_cnn.CreateCommand())
+                {
+                    //执行sql语句
+                    ole_cmd.CommandText = sql;
+                  //执行语句--接受数据
+                    using (OleDbDataAdapter dapter = new OleDbDataAdapter(ole_cmd))
+                    {
+                        //创建对应内存表格结构数据-为了接受返回表格
+                        DataSet dr = new DataSet();//创建空白表格
+                        //填充表格数据
+                        dapter.Fill(dr);
+                        //返回查询到的内容
+                        return dr.Tables[0];
+                    }
+                }
+            }
+        }
+    }
+}
